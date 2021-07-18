c04741fd0088c859660dc4feba3b20a969c5b50f
ahsan70 <ahsan-ali70@outlook.com>
2021-07-18 08:01:55 -0700

    Added Custome Html Helper

---
+++ MVCWebAppFromScratch/CustomeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebAppFromScratch
+{
+    public static class CustomeHelper
+    {
+        public static IHtmlString Image(string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>", src, alt));
+
+        }
+        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>", src, alt));
+
+        }
+        public static IHtmlString ImageTag(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tag = new TagBuilder("img");
+            tag.Attributes.Add("src",src);
+            tag.Attributes.Add("alt",alt);
+
+
+            return new MvcHtmlString(tag.ToString());
+
+        }
+    }
+}
