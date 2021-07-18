ed6defbc5a5e29d683fe1bc74e4e743458eb27b3
ahsan70 <ahsan-ali70@outlook.com>
2021-07-18 06:07:10 -0700

    Added Inline Helper and Strongly Type Helper

--- MVCWebAppFromScratch/Controllers/HomeController.cs
+++ MVCWebAppFromScratch/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCWebAppFromScratch.Models;
 
 namespace MVCWebAppFromScratch.Controllers
 {
@@ -11,12 +12,30 @@ namespace MVCWebAppFromScratch.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var data = GetEmployee();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+
             return View();
         }
 
         public ViewResult AboutUs()
         {
             return View();
+        }
+
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+               Id = 1,
+               Name = "Ahsan",
+               Address = "Dallas Texas"
+
+            };
         }
     }
 }
